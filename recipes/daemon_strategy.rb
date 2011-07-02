afe2c8ebd4fdb44e9627b4f833b04db6014ad0b4
Dmitriy Kiriyenko <dmitriy.kiriyenko@gmail.com>
2011-07-02 13:03:31 +0300

    Default daemon is passenger now.

--- recipes/daemon_strategy.rb
+++ recipes/daemon_strategy.rb
@@ -1,5 +1,5 @@
 on :load do
-  strategy = fetch(:daemon_strategy, :mongrel_cluster)
+  strategy = fetch(:daemon_strategy, :passenger)
 
   if [:passenger, :mongrel_cluster].include? strategy
     load File.join(File.dirname(__FILE__), "daemon_strategies", "#{strategy}.rb")
