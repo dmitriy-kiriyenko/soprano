e2837bdc8164ee6530e591112a97eefc41af4b87
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2012-03-31 12:28:25 +0300

    Add unicorn support.

---
+++ lib/soprano/unicorn.rb
@@ -0,0 +1,28 @@
+Capistrano::Configuration.instance(:must_exist).load do
+  def unicorn_conf
+    fetch(:unicorn_conf, 'config/unicorn.rb')
+  end
+
+  def unicorn_bin
+    fetch(:unicorn_bin, 'bundle exec unicorn')
+  end
+
+  namespace :deploy do
+    desc "Start application."
+    task :start, :roles => :app do
+      run "bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D"
+    end
+
+    desc "Stop application."
+    task :stop, :roles => :app do
+      run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+    end
+
+    desc "Restart application."
+    task :restart, :roles => :app do
+      run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -HUP `cat #{unicorn_pid}`; else cd #{deploy_to}/current && #{unicorn_bin} -c #{unicorn_conf} -E #{rails_env} -D; fi"
+    end
+  end
+
+  depend :remote, :gem, "passenger", ">=2.2.2"
+end
