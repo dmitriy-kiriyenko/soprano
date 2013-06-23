537a66028f071e70d0b40cf770764536004b7db2
Alexey Osipenko <alexey@osipenko.in.ua>
2013-06-23 10:08:17 +0300

    The puma server configuration

---
+++ lib/soprano/puma.rb
@@ -0,0 +1,58 @@
+Capistrano::Configuration.instance(:must_exist).load do
+
+  def puma_sock
+    fetch :puma_sock, 'unix://#{File.join([shared_path, "puma.sock"])}'
+  end
+
+  def puma_conf
+    fetch :puma_conf, File.join [shared_path, "config", "puma.rb"]
+  end
+
+  def puma_pid
+    fetch :puma_pid, File.join([shared_path, "pids", "puma.pid"])
+  end
+
+  def puma_state
+    fetch :puma_state, File.join([shared_path, "puma.state"])
+  end
+
+namespace :deploy do
+  desc "Start application."
+  task :start, :roles => :app do
+    run "service puma start #{application}"
+  end
+
+  desc "Stop application."
+  task :stop, :roles => :app do
+    run "service puma stop #{application}"
+  end
+
+  desc "Restart application."
+  task :restart, :roles => :app do
+    run "service puma restart #{application}"
+  end
+
+  desc "Creating config for application"
+  task :init_config, :roles => :app do
+    db_config = ERB.new <<-EOF
+app_path = "#{deploy_to}"
+shared_path = "#{shared_path}"
+
+environment "production"
+
+threads 2,2
+
+bind "#{puma_sock}"
+pidfile "#{puma_pid}"
+state_path "#{puma_state}"
+
+activate_control_app
+    EOF
+
+    run "mkdir -p #{File.dirname(puma_pid)}"
+    put db_config.result, puma_conf
+  end
+
+end
+
+end
